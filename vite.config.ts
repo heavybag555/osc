07457a8b0676b5a8363c4488910ab57a4b5ec8f4
heavybag555 <bkuribe313@gmail.com>
2025-11-19 14:28:45 -0800

    blah

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from "@vitejs/plugin-react";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/osc/", // GitHub Pages base path (matches repository name)
+  base: "/temp-project/", // GitHub Pages base path (matches repository name)
   server: {
     host: "localhost",
     port: 5173,
